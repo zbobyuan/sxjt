6e46f2780f238ae2066cfc7dad16e84948d4b343
bobyuan <16703306@qq.com>
2016-02-11 20:43:50 +0800

    initial checkin. use Microsoft.VisualBasic.FileIO.TextFieldParser to parse csv file, supports newline in cell.

---
+++ XSalaryFormatter/Main.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace XSalaryFormatter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lblSalarySummary = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "CSV格式工资表|*.csv";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(13, 13);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(189, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "打开csv格式工资表";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Enabled = false;
+            this.btnConvert.Location = new System.Drawing.Point(223, 13);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(140, 23);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "转换为可导入格式";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(390, 13);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 92);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(711, 337);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // lblSalarySummary
+            // 
+            this.lblSalarySummary.AutoSize = true;
+            this.lblSalarySummary.Location = new System.Drawing.Point(13, 58);
+            this.lblSalarySummary.Name = "lblSalarySummary";
+            this.lblSalarySummary.Size = new System.Drawing.Size(41, 12);
+            this.lblSalarySummary.TabIndex = 4;
+            this.lblSalarySummary.Text = "label1";
+            this.lblSalarySummary.Visible = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 441);
+            this.Controls.Add(this.lblSalarySummary);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnOpen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "交干校工资预处理程序";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label lblSalarySummary;
+    }
+}
---
+++ XSalaryFormatter/Main.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XSalaryFormatter
+{
+    public partial class Main : Form
+    {
+        private Salary _salary;
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile(string strFileName)
+        {
+            btnConvert.Enabled = btnSave.Enabled = false;
+            try
+            {
+                _salary = Salary.ReadFromFile(strFileName);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "打开文件出错", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Render();
+            btnConvert.Enabled = true;
+        }
+
+        private void Render()
+        {
+            lblSalarySummary.Visible = true;
+            if (string.IsNullOrEmpty(_salary.Header) || _salary.Rows.Count == 0)
+            {
+                richTextBox1.Clear();
+                richTextBox1.AppendText(_salary.RawText);
+                lblSalarySummary.Text = string.Format("文件：{0}。", _salary.FileName);
+                return;
+            }
+
+            
+            lblSalarySummary.Text = string.Format("文件：{0}, 包含{1}名职工的工资。", _salary.FileName, _salary.Rows.Count);
+            richTextBox1.Clear();
+            richTextBox1.AppendText(_salary.Header + Environment.NewLine);
+            foreach (string row in _salary.Rows)
+            {
+                richTextBox1.AppendText(row + Environment.NewLine);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                OpenFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (_salary == null)
+            {
+                return;
+            }
+            _salary.Header = null;
+            _salary.Rows.Clear();
+
+            using (Stream stream = GenerateStreamFromString(_salary.RawText))
+            {
+                TextFieldParser parser = new TextFieldParser(stream);
+                parser.HasFieldsEnclosedInQuotes = true;
+                parser.SetDelimiters(",");
+
+                string[] fields;
+                int counter = 0;
+                while (!parser.EndOfData)
+                {
+                    fields = parser.ReadFields();
+                    if (fields == null || fields.Length == 0)
+                        continue;
+                    if (counter == 0)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        for (int i = 0; i < fields.Length; i++)
+                        {
+                            string field = fields[i].Trim().Replace(" ", "").Replace("　", "").Replace("\n", "").Replace("\r", "");
+                            field = field.Replace("\"", "\"\"");
+                            if (i != fields.Length - 1)
+                            {
+                                sb.AppendFormat("\"{0}\",", field);
+                            }
+                            else
+                            {
+                                sb.AppendFormat("\"{0}\"", field);
+                            }
+                        }
+                        _salary.Header = sb.ToString();
+                        counter++;
+                    }
+                    else
+                    {
+                        string name = fields[0].Trim().Replace(" ", "").Replace("　", "").Replace("\n", "");
+                        if (name.StartsWith("\"") && name.EndsWith("\""))
+                        {
+                        }
+                        else
+                        {
+                            name = "\"" + name.Replace("\"", "\"\"") + "\"";
+                        }
+                        fields[0] = name;
+                        _salary.Rows.Add(string.Join(",", fields));
+                    }
+                }
+            }
+
+            Render();
+            btnSave.Enabled = true;
+        }
+
+        public Stream GenerateStreamFromString(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (_salary == null) {
+                return;
+            }
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(File.Open(_salary.FileName, FileMode.Truncate), Encoding.GetEncoding("gb2312")))
+                {
+                    sw.WriteLine(_salary.Header);
+                    foreach (string row in _salary.Rows)
+                    {
+                        sw.WriteLine(row);
+                    }
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "保存文件出错", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            MessageBox.Show("保存成功，请关闭本软件并在OA系统中上传工资表。");
+        }
+    }
+
+    class Salary
+    {
+        public static Salary ReadFromFile(string fileName)
+        {
+            //line = line.Trim().Replace(" ", "").Replace("　", "").Replace("\n", "");
+            return new Salary(fileName) { RawText = File.ReadAllText(fileName, Encoding.GetEncoding("gb2312")) };
+        }
+        public Salary(string fileName)
+        {
+            FileName = fileName;
+            Rows = new List<string>();
+        }
+        public string FileName { get; set; }
+        public string Header { get; set; }
+        public List<string> Rows { get; private set; }
+        public string RawText { get; set; }
+    }
+}
